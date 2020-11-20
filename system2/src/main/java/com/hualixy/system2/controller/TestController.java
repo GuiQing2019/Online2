33cac8f21efa1db58b317f4230acbaceeee7bb4f
GuiQing2019 <758426958@qq.com>
2020-11-20 15:07:06 +0800

    将System2注册到eureka2服务中心

--- system2/src/main/java/com/hualixy/system2/controller/TestController.java
+++ system2/src/main/java/com/hualixy/system2/controller/TestController.java
@@ -2,11 +2,25 @@ package com.hualixy.system2.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/** 
+* @Description:
+ *              @RestController = @RestController注解相当于@ResponseBody ＋ @Controller合在一起的作用
+ *                 返回的是jsp的是controller层的字符内容。
+ *              @Controller :
+ *                 返回的是jsp页面。
+* @Author: GuiQingChen
+* @Date: 2020/11/20
+ */
 @RestController
 public class TestController {
 
-
+   /**
+   * @Description: test方法
+   * @Param: []
+   * @return: java.lang.String
+   * @Author: GuiQingChen
+   * @Date: 2020/11/20
+    */
     @RequestMapping("/test")
     public String test(){
         return "succeed";
