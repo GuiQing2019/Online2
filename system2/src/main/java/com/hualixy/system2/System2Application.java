da98c7792de4392ad5873da958c23732c614b71d
GuiQing2019 <758426958@qq.com>
2020-11-20 11:51:45 +0800

    1.增加system客户中心,配置中心、日志文件 2.添加客户中心依赖以及springboot依赖 3.书写controller测试通过中心访问controller层

---
+++ system2/src/main/java/com/hualixy/system2/System2Application.java
@@ -0,0 +1,24 @@
+package com.hualixy.system2;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+
+@SpringBootApplication
+public class System2Application {
+
+private static final Logger LOG= LoggerFactory.getLogger(System2Application.class);
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(System2Application.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功!");
+        LOG.info("System地址：\t127.0.0.1:{}",env.getProperty("server.port"));
+    }
+
+}
---
+++ system2/src/main/java/com/hualixy/system2/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.hualixy.system2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/test")
+    public String test(){
+        return "succeed";
+    }
+}
